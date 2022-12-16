691ddab4a84f944e0ccef4854d73967ea915a059
Lavanov <alavanov@rencap.com>
2022-12-16 16:02:53 +0700

    view.py

---
+++ view.py
@@ -0,0 +1,13 @@
+
+def view_data(data, title):
+    print(f'{title} = {data}')
+
+def get_contact_info():
+    return input()
+
+def ask_to_fill(text):
+    print(f"\nВведите {text} :")
+
+def print_list(printed_list):
+    for i in printed_list:
+        print(",".join(map(str,i)))
